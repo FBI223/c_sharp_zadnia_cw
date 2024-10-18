e6d32da15a2deeee44fd8b74ef60227a69afc9ec
Marcin_FBI <88114702+FBI223@users.noreply.github.com>
2024-10-18 13:12:29 +0200

    small fixes

--- 1/zad_1/obj/Debug/net8.0/zad_1.AssemblyInfo.cs
+++ 1/zad_1/obj/Debug/net8.0/zad_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zad_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21235bbeb3c77c2af93a64b5606392b4551630a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca6972cc9af84548f42c73ac7f1d4de689dcaaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("zad_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zad_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
